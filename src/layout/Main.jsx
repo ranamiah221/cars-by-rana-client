e913caa5673b128f723b01d0d86b178ca84bfab5
ranamiah221 <ranarasul21@gmail.com>
2023-12-11 21:20:53 +0600

    added navbar and footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/sharad/Navbar/Navbar";
+import Footer from "../pages/sharad/Footer/Footer";
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
